3db54d06dfb0b1fd3a2d3a50024df85c50a12b5a
Jackal_lzt <bkyarokun@gmail.com>
2023-05-09 17:14:37 +0800

    fix(main): fix error prompt when failed to convert

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const app = express();
 const converter = require("liquid-to-handlebars");
-const bodyParser = require("body-parser");
 
-// 使用 body-parser 中间件解析请求体
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 // 首页
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
