eadeeb95f6dc7adfd362a0821247386063bfee58
Jackal_lzt <bkyarokun@gmail.com>
2023-05-09 16:27:48 +0800

    feat(main): achieve liquid-to-handlebars repo, add styling, add regex double replacement

--- index.js
+++ index.js
@@ -11,21 +11,6 @@ app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
 
-// 处理 POST 请求
-app.post("/process", (req, res) => {
-  // 获取请求体中的数据
-  const inputData = req.body.data;
-
-  // 替换字符串中的 var 为 let
-  const processedData = converter.convert(inputData);
-
-  // 解码字符串中的转义字符
-  const decodedData = decodeURIComponent(processedData);
-
-  // 发送处理后的字符串作为响应
-  res.send(decodedData);
-});
-
 // 启动服务器
 app.listen(5000, () => {
   console.log("Server started on port 5000");
